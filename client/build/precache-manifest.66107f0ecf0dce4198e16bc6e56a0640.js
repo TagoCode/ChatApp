a5d98d22208654e18531c9ac2416940d0d13e27c
Tash Dutton <tashdutton@gmail.com>
2020-10-30 11:40:16 -0700

    ChatApp Mk2

---
+++ client/build/precache-manifest.66107f0ecf0dce4198e16bc6e56a0640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed88f060e7846cf89474076776d03a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd501423e3f69678c30",
+    "url": "/static/css/main.2727df46.chunk.css"
+  },
+  {
+    "revision": "b9234484ceb835b69006",
+    "url": "/static/js/2.b1e1874b.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.b1e1874b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd501423e3f69678c30",
+    "url": "/static/js/main.cff99797.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
